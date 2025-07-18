b6645641442eda43f9cacb8e9f788b61e3f39c4a
Tanish Khanna <74340415+Tanish-Khanna@users.noreply.github.com>
2025-07-18 11:05:43 -0700

    Add debug-controlled logging for random word

--- randomWord.py
+++ randomWord.py
@@ -1,9 +1,14 @@
 import random
+import logging
+import config
+
+logger = logging.getLogger(__name__)
 
 
 def randomWord():
     with open("wordlist/filtered_oxford_3000.txt", "r") as file:
         word_list = file.read().splitlines()
         a = random.choice(word_list)
-        print ("the random word is " + a)
+        if config.DEBUG_RANDOM_WORD:
+            logger.debug("the random word is %s", a)
     return a
